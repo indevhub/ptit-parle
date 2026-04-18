b94f053e1df7c9ed9a723f5a043cf1e6176ad5fd
Bild Pruf <bildproof@gmail.com>
2026-04-18 20:36:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1460655704-9a7ea",
+  "appId": "1:110725614669:web:6b4b446dd993ac56a12004",
+  "apiKey": "AIzaSyAIA5AVij8XjCkbN-FL6CI55sERmIQzPEc",
+  "authDomain": "studio-1460655704-9a7ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "110725614669"
+};
